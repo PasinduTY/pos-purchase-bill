e3f7766097f09ad7166c886491eb26880595f68e
PasinduTY <pasindu.t.y.s@gmail.com>
2026-07-12 21:48:54 +0530

    Add purchase bill placeholder page and routing

--- frontend/purchase-bill-frontend/src/app/app.routes.ts
+++ frontend/purchase-bill-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
+import { PurchaseBill } from './features/purchase-bill/purchase-bill';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'purchase-bill', component: PurchaseBill },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ frontend/purchase-bill-frontend/src/app/features/purchase-bill/purchase-bill.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-bill',
+  imports: [],
+  templateUrl: './purchase-bill.html',
+  styleUrl: './purchase-bill.css',
+})
+export class PurchaseBill {
+
+}
